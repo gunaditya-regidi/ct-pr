bbde7ec313fb858962a5711ce0f27aeae64abee5
gunaditya-regidi <regidigunaditya@gmail.com>
2025-10-03 17:03:03 +0530

    added s

--- app.py
+++ app.py
@@ -813,4 +813,4 @@ def images(filename):
     return send_from_directory(os.path.join('static', 'images'), filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
